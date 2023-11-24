545602539a53fde3eeba853b12c5ce338078ca9c
berkleylim <berkleylim16@gmail.com>
2023-11-24 21:42:29 +0900

    fix: ver03 에러 수정 - app.get과 app.listen 보다 더 위에 함수로 지정해야합니다.

--- ver03/index.js
+++ ver03/index.js
@@ -5,6 +5,19 @@ const express = require("express");
 const app = express();
 const PORT = 300;
 
+// -------------------------------------------------------------------
+// 이 함수가 get보다 더 위에 올라와져있어야 한다.
+// 서버 실행시 화면 표시
+const consoleListening = () => {
+  console.log(`server start : http://localhost:${PORT}`);
+}
+
+// 인덱스에서 연결하는 함수
+const mainView = (request, response) => {
+  response.send('main 화면입니다.');
+}
+// -------------------------------------------------------------------
+
 // 경로 설정
 // 경로가 / 일때 main 화면입니다. 실행
 app.get('/', mainView)
@@ -14,12 +27,3 @@ app.get('/', mainView)
 // console에 server start : http://localhost:300 올리기
 app.listen(PORT, consoleListening);
 
-// 서버 실행
-const consoleListening = () => {
-  console.log(`server start : http://localhost:${PORT}`);
-}
-
-// 인덱스에서 연결하는 함수
-const mainView = (request, response) => {
-  response.send('main 화면입니다.');
-}
