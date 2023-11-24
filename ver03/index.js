ded8342779499a4c1be456745595c7f3e7dd9b99
berkleylim <berkleylim16@gmail.com>
2023-11-24 21:35:30 +0900

    feat: ver03 설정 - express 설정하고, console 창과 main 라우터 설정

---
+++ ver03/index.js
@@ -0,0 +1,25 @@
+// express 선언
+const express = require("express");
+
+// express에 app 넣고, 포트 지정
+const app = express();
+const PORT = 300;
+
+// 경로 설정
+// 경로가 / 일때 main 화면입니다. 실행
+app.get('/', mainView)
+
+// 서버 실행
+// main 화면입니다. 결과 뜨고,
+// console에 server start : http://localhost:300 올리기
+app.listen(PORT, consoleListening);
+
+// 서버 실행
+const consoleListening = () => {
+  console.log(`server start : http://localhost:${PORT}`);
+}
+
+// 인덱스에서 연결하는 함수
+const mainView = (request, response) => {
+  response.send('main 화면입니다.');
+}
