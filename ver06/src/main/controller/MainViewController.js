4eba47b0427387435d518e7254997635e92f1f1b
berkleylim <berkleylim16@gmail.com>
2023-11-26 14:39:43 +0900

    chron: ver06 프로젝트 셋팅 - ver05 기반으로 이어서 진행 - 현재 환경 설정은 ver05 프로젝트 기반으로 사용함 - 따라서, 미리 셋팅 후 작업 진행함

---
+++ ver06/src/main/controller/MainViewController.js
@@ -0,0 +1,5 @@
+exports.mainView = (request, response, next) => {
+  response.send('main 화면입니다.')
+};
+
+  
---
+++ ver06/src/main/controller/StoreController.js
@@ -0,0 +1,4 @@
+exports.storeView = (request, response, next) => {
+  response.send('매장 정보 입니다!!!')
+};
+  
