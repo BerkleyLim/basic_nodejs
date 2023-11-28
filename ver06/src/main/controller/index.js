368fd70dfc5d85abf2cc31a00cc9471cfcc02201
berkleylim <berkleylim16@gmail.com>
2023-11-28 21:43:45 +0900

    feat: cors 문제 해결 - 전체 api 사용을 위해 특정 api에 적용 - 현재는 특정 host가 아닌 전체로 지정함

--- ver06/src/main/controller/index.js
+++ ver06/src/main/controller/index.js
@@ -4,8 +4,18 @@ const MainViewController = require('./MainViewController');
 const MemberController = require('./MemberController');
 const StoreController = require('./StoreController');
 
-router.get('/', MainViewController.mainView)
-router.get('/memberView/', MemberController.memberView)
-router.get('/storeView/', StoreController.storeView)
+// cors 해결
+const cors = require('cors');
+
+// 아래는 특정 host만 허용 (리액트 프로젝트 적용전)
+// const corsOption = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200
+// }
+
+
+router.get('/', cors(), MainViewController.mainView)
+router.get('/memberView/', cors(), MemberController.memberView)
+router.get('/storeView/', cors(), StoreController.storeView)
 
 module.exports = router;
