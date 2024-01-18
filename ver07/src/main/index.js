1cfc56576c6ff118db7bd2976ee1e6342b7eb7cf
berkleylim <berkleylim16@gmail.com>
2024-01-18 12:28:28 +0900

    fix: body is null point exception on request body - Post 호출 시 request body null 값 뜨는 현상 해결

--- ver07/src/main/index.js
+++ ver07/src/main/index.js
@@ -1,6 +1,9 @@
 // express 선언
 const express = require("express");
 
+// POST에 request body 넣기 위해 실행
+const bodyParser = require('body-parser')
+
 // express에 app 넣고, 포트 지정
 const app = express();
 const PORT = 300;
@@ -8,6 +11,9 @@ const http = require('http').createServer(app);
 
 // api 지정 (router)
 const routes = require('./controller')
+
+// body - parser
+app.use(bodyParser.json)
 
 app.use(routes)
 // 서버 실행시 화면 표시
