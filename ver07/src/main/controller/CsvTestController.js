82aeca1a6d5452b957c36419ef45d22cfea40506
berkleylim <berkleylim16@gmail.com>
2024-01-19 17:25:32 +0900

    fix: CsvTextController.js - CsvText -> CsvTest로 변경 - 철자 틀려서 변경함 - 파일 연동은 아직 에러 발생 - 이부분은 수정 에정

--- ver07/src/main/controller/CsvTestController.js
+++ ver07/src/main/controller/CsvTestController.js
@@ -1,22 +1,64 @@
-const csv = reqire('csvtojson')
+const csv = require("csvtojson");
 
-const memberFilePath = __dirname + "../../resource/csv/member.csv"
-const groupFilePath = __dirname + "../../resource/csv/group.csv"
+const memberFilePath = __dirname + "../../resource/csv/member.csv";
+const groupFilePath = __dirname + "../../resource/csv/group.csv";
 
 // 참조 : https://ooeunz.tistory.com/26
-exports.CsvText = async (request, response, next) => {
+exports.CsvTest = async (request, response, next) => {
   try {
-    // 여기는 express를 사용한다.
+    const member = await csv().fromFile(memberFilePath);
+    const group = await csv().fromFile(groupFilePath);
 
-    // csv 파일 읽어오기
-    try{
-      const member = await csv().fromFile(memberFilePath)
-    } catch{(e) => console.log(e)}
+    if (!member || !group) console.log(`file read err : ${err}`); // debug
 
-    response.json({pageNm: 'main 화면'})
+    IDX = req.params.groupIdx;
+    let groupNum = group[String(Number(IDX - 1))].name;
+
+    let people = member
+      .filter((it) => it.groupIdx === IDX)
+      .map((it) => it.name);
+
+    res.send(`${groupNum} : ${people}`);
   } catch (err) {
-    console.log(err)
+    console.log(`err with csv : ${err}`);
   }
 };
 
-  
+// group.js
+
+/**
+ * 
+// find group member
+router.get('/:groupIdx', async (req, res) => {
+    try {
+        const member = await csv().fromFile(memberFilePath);
+        const group = await csv().fromFile(groupFilePath);
+
+        if (!member || !group) console.log(`file read err : ${err}`);   // debug     
+
+        IDX = req.params.groupIdx;
+        let groupNum = group[String(Number(IDX - 1))].name;
+
+        let people = member.filter(it => it.groupIdx === IDX).map(it => it.name);
+        
+        res.send(`${groupNum} : ${people}`);
+    } catch (err) {
+        console.log(`err with csv : ${err}`);
+    }
+});
+
+// find all
+router.get('/', async (req, res) => {
+    try {
+     const member = await csv().fromFile(memberFilePath);
+
+        if (!member) console.log(`file read err : ${err}`); // debug   
+        else res.send(member);  // running
+
+    } catch (err) {
+        console.log(`file read err : ${err}`);
+    }
+});
+
+module.exports = router;
+ */
