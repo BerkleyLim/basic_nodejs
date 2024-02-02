273b176b50ba74e444ded4f7d879c8f1942f91ee
berkleylim <berkleylim16@gmail.com>
2024-02-02 17:34:02 +0900

    refactor: main css

--- ver07/src/main/controller/MainViewController.js
+++ ver07/src/main/controller/MainViewController.js
@@ -9,37 +9,6 @@ exports.mainView = (request, response, next) => {
 
     if (!main) console.log(`file read err : ${err}`); // debug
 
-    // IDX = request.body.groupIdx;
-    // let groupNum = group[String(Number(IDX - 1))].name;
-
-    // let people = member
-    //   .filter((it) => it.groupIdx === IDX)
-    //   .map((it) => it.name);
-
-    // csv로 변환
-    // const csvResult = jsonToCSV(member);
-    // fs.writeFile('src/resource/csv/test.csv', csvResult, err => {
-    //   if (err) {
-    //     console.log(err.message);
-    
-    //     throw err;
-    //   }
-    
-    //   console.log('data written to file');
-    // });
-
-    
-
-    // response.send(`${groupNum} : ${people}`);
-
-    // let tbody = () => {
-    //   let data = main?.map((data, index) => {
-    //     return `<tr key=${index}><td width="50px">${data?.bno}</td><td width="150px">${data?.title}</td><td width="600px">${data?.contents}</td></tr>`
-    //   })
-    //     console.log(typeof(data))
-    //     console.log(JSON.stringify(data))
-    //     return JSON.stringify(data);
-    //   }
     let html = `
       <div style="display:flex; width:800px; text-align:center">
         <table>
