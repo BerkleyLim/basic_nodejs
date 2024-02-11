3bbda1eb4c0d205076de7de09e90de8dde648124
berkleylim <berkleylim16@gmail.com>
2024-02-11 20:09:30 +0900

    feat: catModuls, usersmodules 설치 - API 설치함 - REST API 버전으로 설치 - 향후 직접 DB 연동도 해보기

--- ver09/board-back/src/app.module.ts
+++ ver09/board-back/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CatsModule } from './cats/cats.module';
 
 // 애플리케이션 루트 모듈, 각 모듀들은 이 파일에 import함
 @Module({
-  imports: [],
+  imports: [UsersModule, CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
