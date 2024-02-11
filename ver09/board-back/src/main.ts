cea32bbaeca9fe77674df0e0a554e6ed870d0220
berkleylim <berkleylim16@gmail.com>
2024-02-11 15:34:28 +0900

    comment: descript to begin nestjs point - 시작점 주석 작성

--- ver09/board-back/src/main.ts
+++ ver09/board-back/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// nest.js의 시작 점
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
