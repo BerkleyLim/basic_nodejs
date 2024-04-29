16b6f544221bc95b5b5a200ea70c615399ff5767
BerkleyLim <berkleylim16@gmail.com>
2024-04-29 21:55:28 +0900

    style: set formatter, comment
    - prettier 형식 지정
    - 주석 추가

--- ver09/board-back/src/board/board.controller.ts
+++ ver09/board-back/src/board/board.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
--- ver09/board-back/src/board/board.service.ts
+++ ver09/board-back/src/board/board.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { Board } from './entities/board.entity';
@@ -8,17 +8,24 @@ export class BoardService {
   private board: Array<Board> = [];
   private bno = 0;
 
+  /**
+   * 다음은 게시판을 생성한다.
+   * @param createBoardDto : dto 클래스
+   */
   create(createBoardDto: CreateBoardDto) {
     this.board.push({ bno: ++this.bno, ...createBoardDto });
   }
 
+  /**
+   * 게시판 전체 출력
+   */
   findAll() {
     return [...this.board];
   }
 
   /**
-   * 주어진 id에 부합하는 데이터가 없는 경우 NotFoundException 예외 던짐
-   * @param id
+   * 다음은, 해당 게시판 인덱스 찾는다.
+   * @param bno : 인덱스 번호
    */
   findOne(bno: number) {
     const found = this.board.find((b) => b.bno === bno);
@@ -26,6 +33,11 @@ export class BoardService {
     return found;
   }
 
+  /**
+   * 다음은 게시판을 수정한다.
+   * @param bno : 인덱스번호
+   * @param updateBoardDto : 필요 dto
+   */
   update(bno: number, updateBoardDto: UpdateBoardDto) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const found = this.board.find((b) => b.bno === bno);
@@ -33,6 +45,10 @@ export class BoardService {
     this.board.push({ ...found, ...updateBoardDto });
   }
 
+  /**
+   * 게시판 삭제
+   * @param bno : 삭제할 게시판 번호
+   */
   remove(bno: number) {
     this.findOne(bno);
     this.board = this.board.filter((b) => b.bno !== bno);
