dd2a4f48ed2cbbc1199e91d659f066ecef531610
BerkleyLim <berkleylim16@gmail.com>
2024-04-29 16:18:53 +0900

    feat: getting start nestjs on Ver09
    - nestjs 연습
    - board 용 api 생성
    - nestjs 와 친해지기

--- ver09/board-back/src/app.module.ts
+++ ver09/board-back/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { CatsModule } from './cats/cats.module';
+import { BoardModule } from './board/board.module';
 
 // 애플리케이션 루트 모듈, 각 모듀들은 이 파일에 import함
 @Module({
-  imports: [UsersModule, CatsModule],
+  imports: [BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
